a0b4014fe3b697d3a8712ce5e8536529ec96699b
carlitoTheDeveloper <carlosneira1997@gmail.com>
2018-05-16 09:48:17 -0400

    Adding the face recognition Trainer

--- trainer.py
+++ trainer.py
@@ -8,6 +8,21 @@ path='dataSet'
 
 def getImagesWithID(path):
     imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
-    print imagePaths
+    faces=[]
+    IDs=[]
 
-getImagesWithID(path)
+    for imagePath in imagePaths:
+        faceImg=Image.open(imagePath).convert('L');
+        faceNp=np.array(faceImg,'uint8')
+        ID=int(os.path.split(imagePath)[1].split('.')[1])
+        faces.append(faceNp)
+        print ID
+        IDs.append(ID)
+        cv2.imshow("Training",faceNp)
+        cv2.waitKey(10)
+    return IDs, faces
+
+Ids,faces=getImagesWithID(path)
+recognizer.train(faces,np.array(Ids))
+recognizer.save('recognizer/trainingData.yml')
+cv2.destroyAllWindows()
